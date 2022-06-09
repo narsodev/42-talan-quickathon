1d1ff475891e736a0b264b026f56a1be921411c4
narsodev <narsodev@gmail.com>
2022-06-09 17:18:47 +0200

    Add shop

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,45 @@ import QrReader from 'react-web-qr-reader'
 function App() {
   const [count, setCount] = useState(0)
   const [scan, setScan] = useState(false)
-  const [data, setData] = useState('')
+
+  const handleShowCam = () => {
+	setScan(scan => !scan)
+  }
+
+  const handleScan = result => {
+	const { data } = result
+	const number = Number(data.split(":")[1])
+	if (!Number.isNaN(number)) {
+		setCount(Number(count) + Number(result.data.split(":")[1]))
+		alert(`¡Sumados ${number} EspetoCoins!`)
+	}
+	else
+		alert('Código no válido')
+	setScan(false)
+  }
+
+  const handleError = error => {
+	console.error(error)
+	alert(error)
+	setScan(false)
+  }
+
+  const camStyle = {
+	height: 240,
+	width: 320
+  }
+
+  const handleBuy = (price, item) => {
+	const number = Number(price)
+	if (Number.isNaN(Number) || count < number)
+	{
+		alert('Lo siento eres pobre')
+		return
+	} else {
+		setCount(count - number)
+		alert(`¡Disfruta de tu ${item}!`)
+	}
+  }
 
   return (
 	<>
@@ -24,35 +62,31 @@ function App() {
 			<img src="https://1001beach.com/img/posts/3514/1200/playa_de_maro-2.jpg" alt="Playa de Maro" />
 			<section className='form'>
 				<h2>¿Quieres conseguir puntos?</h2>
-				<button onClick={() => setScan(scan => !scan)}>Registrar QR</button>
+				<button className='button-scan' onClick={handleShowCam}>Registrar QR</button>
 				{
 					scan && <QrReader
 					className='qr'
-					onScan={result => {
-						const { data } = result
-						const number = Number(data.split(":")[1])
-						if (!Number.isNaN(number)) {
-							setCount(Number(count) + Number(result.data.split(":")[1]))
-							alert(`¡Sumados ${number} EspetoCoins!`)
-						}
-						else
-							alert('Código no válido')
-						setScan(false)
-					}}
-					onError={error => {
-						console.error(error)
-						alert(error)
-						setScan(false)
-					}}
-					style={
-						{
-							height: 240,
-							width: 320
-						}
-					}
+					onScan={handleScan}
+					onError={handleError}
+					style={camStyle}
 					/>
 				}
-				
+			</section>
+			<section className='shop'>
+				<button onClick={() => handleBuy(20, 'mojito')}>
+					<article>
+						<img src="https://www.comedera.com/wp-content/uploads/2022/04/mojito.jpg" alt="Mojito" />
+						<h3>Mojito</h3>
+						<h4>20 <IconEspeto /></h4>
+					</article>
+				</button>
+				<button onClick={() => handleBuy(50, 'kayak')}>
+					<article>
+						<img src="https://m.media-amazon.com/images/I/61usYLIMwdL._AC_SL1200_.jpg" alt="Kayak" />
+						<h3>Kayak</h3>
+						<h4>50 <IconEspeto /></h4>
+					</article>
+				</button>
 			</section>
 		</main>
 		<footer>
